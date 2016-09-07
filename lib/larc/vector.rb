5ece8bbc1b5f8ecc08e922be048df368ead52cdf
Jonathan Hoyt <hoyt@github.com>
2016-09-06 21:08:45 -0700

    Add public interface for creating vector and tests/test harness for testing

--- lib/larc/vector.rb
+++ lib/larc/vector.rb
@@ -1,4 +1,16 @@
 module Larc
   class Vector
+
+    # Public: Primary interface to instantiate a vector with coordinates.
+    def self.[](*coordinates)
+      new(coordinates: coordinates)
+    end
+
+    def initialize(coordinates:)
+      @coordinates = coordinates.map {|coordinate| coordinate.to_f }
+      @dimension = coordinates.length
+    end
+
+    attr_reader :coordinates, :dimension
   end
 end
